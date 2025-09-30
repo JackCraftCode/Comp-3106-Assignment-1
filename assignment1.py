27da9f54ba74762a60d579630aa2117625ebe386
Jackson McAfee <jacksonminecraft88@gmail.com>
2025-09-29 20:19:33 -0400

    Changed indent sizing

--- assignment1.py
+++ assignment1.py
@@ -2,31 +2,31 @@
 import csv
 # The pathfinding function must implement A* search to find the goal state
 def pathfinding(filepath):
-  # filepath is the path to a CSV file containing a grid 
-  grid = []
-  with open(filepath, newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-      grid.append(row)
-  m, n = len(grid), len(grid[0])
+	# filepath is the path to a CSV file containing a grid
+	grid = []
+	with open(filepath, newline='') as f:
+		reader = csv.reader(f)
+		for row in reader:
+			grid.append(row)
+	m, n = len(grid), len(grid[0])
 
-  start = None
-  goals = []
-  treasures = []
-  
-  for r in range(m):
-    for c in range(n):
-      tempValue = grid[r][c] 
-      if tempValue == 'S':
-        start = (r, c) // stores as (x, y)
-      elif tempValue == 'G':
-        goals.append((r,c)) // stores as (x, y) in goals list
-      elif val.isdigit() and int(tempValue) > 0:
-        treasures.append((r,c, int(tempValue))) // stores as (x, y value) in treasures list
+	start = None
+	goals = []
+	treasures = []
 
-        
+	for r in range(m):
+		for c in range(n):
+			tempValue = grid[r][c]
+			if tempValue == 'S':
+				start = (r, c) # stores as (x, y)
+			elif tempValue == 'G':
+				goals.append((r,c)) # stores as (x, y) in goals list
+			elif val.isdigit() and int(tempValue) > 0:
+				treasures.append((r,c, int(tempValue))) # stores as (x, y value) in treasures list
 
-  # optimal_path is a list of coordinate of squares visited (in order)
-  # optimal_path_cost is the cost of the optimal path
-  # num_states_explored is the number of states explored during A* search
-  return optimal_path, optimal_path_cost, num_states_explored
+
+
+	# optimal_path is a list of coordinate of squares visited (in order)
+	# optimal_path_cost is the cost of the optimal path
+	# num_states_explored is the number of states explored during A* search
+	return optimal_path, optimal_path_cost, num_states_explored
